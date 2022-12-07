c5e185f0c0fd736526faf9888edacc61db6fe2c2
Ming Tang <31554062+tangming2008@users.noreply.github.com>
2022-12-06 22:45:06 -0800

    Update app.py
    replace the app

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from matplotlib.pylab import *
 import numpy as np
+import pandas as pd
 
-# Process conditions
+# mu = st.slider('mu - mean', min_value = 0, max_value = 2, step=1, value = 1)
+# sigma = st.slider('sigma - variance', min_value = 0, max_value = 2, step=1, value = 1)
+# size = st.slider('size - counts', min_value = 0, max_value = 2000, step=100, value = 100)
+# data = np.random.normal(mu, sigma, size=size)
+# fig, ax = plt.subplots()
+# ax.hist(data, bins=20)
+# st.pyplot(fig)
 
 widthAv = st.slider('Ave melt-pool width', min_value = 0, max_value = 100, step=10, value = 50)
+# Process conditions
 # widthAv = 60.0   # average melt pool half width
 widthdev = 0.001    # relative standard deviation of width
-dwratioAv = 0.0   # average (D-R)/W ratio of melt pool
+dwratioAv = 0.309   # average (D-R)/W ratio of melt pool
 dwdev = 0.001       # relative standard deviation of depth:width ratio
-hwratioAv = 0.000   # average R/W ratio of melt pool
+hwratioAv = 0.191   # average R/W ratio of melt pool
 hwdev = 0.001       # relative standard deviation of height:width ratio
 hatch = 110.0     # hatch spacing
 layer = 40.0      # layer thickness
@@ -19,8 +27,8 @@ minplot = 0       # minimum and maximum
 maxplot = 3.5       # values for plot range of number of melting cycles
 
 # Definition of calculation domain
-dims = 501        # lateral dimension of square calculation domain in pixels
-xwidth=500      # lateral dimension of square calculation domain in microns
+dims = 1001        # lateral dimension of square calculation domain in pixels
+xwidth=1000.0      # lateral dimension of square calculation domain in microns
 
 xpixel = xwidth / (dims-1) # pixel size in microns
 dimsplus = dims + int(round((hwratioAv+dwratioAv)*4*widthAv/xpixel,0))
@@ -44,13 +52,16 @@ colhi_l=zeros(dimsplus,dtype=np.int)    # as collo, but last column
 # matrix for drawing color scale
 b = zeros((dims,dims//10))
 
-fig = plt.figure(figsize=(8, 6))
+# plt.figure(figsize=(8, 6))
+
+df_all = pd.DataFrame()
 
 
-figure(1,facecolor="white")   # set up window for plotting melt pool shapes
-axes()
+# figure(1,facecolor="white")   # set up window for plotting melt pool shapes
+# axes()
 
 for layercount in range(layers):  # iterate over layers
+
    angle = anglestep * (layercount-3) + angleoffset
    xstep = fabs (hatch / cos(angle))
    originx = -1.0 * np.random.random() * hatch/fabs(cos(angle))  # randomize position of leftmost melt pool
@@ -58,7 +69,9 @@ for layercount in range(layers):  # iterate over layers
    row = dimsplus-int(round(originz/xpixel,0)) # position of top of current layer in pixels
    # print row
    hatchcount = int((xwidth+originx)/hatch*fabs(cos(angle))+ 10) # number of scans across section in current layer
+
    for k in range(hatchcount): # iterate over scans
+#       print(layercount, k)
       column = int(round(originx/xpixel,0))
       hwidth = np.random.normal(widthAv,widthdev*widthAv) # randomize pool half width
       dwratio = np.random.normal(dwratioAv,dwdev*dwratioAv) # randomize pool depth:width ratio
@@ -120,56 +133,38 @@ for layercount in range(layers):  # iterate over layers
             pool[i+180,0]=originx-radius2*cos(calcAngle)
             pool[i,1]=originz+radius1*sin(calcAngle)-(offset-1)*xpixel
             pool[i+180,1]=originz-radius2*sin(calcAngle)-(offset-1)*xpixel
-      poly1=Polygon(pool,facecolor='white',edgecolor='black', linewidth=0.5)
-      gca().add_patch(poly1)  # add melt pool shape to plot
+
+      df = pd.DataFrame(pool)
+      df.columns = ['x', 'y']
+      df['layer'] = layercount
+      df['hatch'] = k
+      df_all = pd.concat([df_all, df])
 
       # originx += fabs (hatch / cos(angle))
       originx += xstep
    originz += layer
 
-#remove transition layers at top and bottom
-a=a[offset:offset+dims,0:dims]
-allAv = np.average(a)
-allMin = np.min(a)
-if allMin == 0:
-   allMin = allAv
-   for i in range(dims):
-      for j in range(dims):
-         if (a[i,j]>0) and (a[i,j]<allMin):
-            allMin = a[i,j]
+# #remove transition layers at top and bottom
+# a=a[offset:offset+dims,0:dims]
+# allAv = np.average(a)
+# allMin = np.min(a)
+# if allMin == 0:
+#    allMin = allAv
+#    for i in range(dims):
+#       for j in range(dims):
+#          if (a[i,j]>0) and (a[i,j]<allMin):
+#             allMin = a[i,j]
 
-# set up plot of melt pool outlines
-axis('scaled')             # ensures correct aspect ratio
-axis([0,xwidth,0,xwidth])  # ranges for melt pool shape plot
-axis('off')                # do not show axes
+df = df_all
+fig, ax = plt.subplots(figsize = (3,3))
+col_groupby = ['layer', 'hatch']
+for index, (name, group) in enumerate(df.groupby(col_groupby)):
+    x, y = group['x'], group['y']
+    ax.fill(x, y, facecolor='grey',edgecolor='black', linewidth=0.5)
+    ax.set_xlim(left=0, right=1000)
+    ax.set_ylim(bottom=0, top=1000)
+    ax.set_aspect('equal', adjustable='box')
 
-#print counts/(dims*dims)
-"average count, expected av, max count, min excluding zero, min"             # average melt count
-round(allAv,2), round(exptAv,2), round(np.max(a),2), round(allMin,2), round(np.min(a),2)
-countsdist = np.histogram(a,bins=20,range=(0,5))
-totalcounts = np.sum(countsdist[0])
-# "Distribution of number of melting cycles:", countsdist[0]*1.0/totalcounts
-# "Bins used to determine distribution:", countsdist[1]
-
-# Plot map of number of melting cycles
-# matshow(a,cmap=cm.afmhot, vmin=minplot, vmax=maxplot) # other possible colormaps: afmhot, coolwarm, bone, gray, RdGy, hot, afmhot
-
-# Calculate and draw color scale
-# for i in range(dims-2):
-#    for j in range(dims//10-2):
-#       b[i,j]=i*1.0/(dims-1)*(maxplot - minplot) + minplot
-#    for j in (0,1,dims//10-2,dims//10-1):
-#       b[i,j]=minplot
-# for i in (dims-3,dims-2,dims-1):
-#    for j in range(dims//10):
-#       b[i,j]=minplot
-
-# matshow(b,cmap=cm.afmhot, vmin=minplot, vmax=maxplot, origin="lower") # other possible colormaps: afmhot, coolwarm, bone, gray, RdGy, hot, afmhot
-# axis('off')
-# scalerange = maxplot-minplot
-# for i in range(int(scalerange+1)):
-#    text(-10,int(round(i/scalerange*(dims-1),0)),str(round(minplot+i,1)),horizontalalignment='right',verticalalignment='center',fontsize=40)
-# text(-10,(dims-1),str(round(maxplot,1)),horizontalalignment='right',verticalalignment='center',fontsize=40)
-
-show()                     # show all figures
+# widthAv = st.slider('Ave melt-pool width', min_value = 0, max_value = 100, step=10, value = 50)
+# plt.savefig('result.png', dpi=600)
 st.pyplot(fig)
